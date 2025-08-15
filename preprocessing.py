ae070c95f75496bc4f8a65fab614efc0eac4ee1c
idonorchidinma <idonor.chidinma@gmail.com>
2025-08-15 13:26:11 -0500

    updated pkl file

--- preprocessing.py
+++ preprocessing.py
@@ -1,4 +1,4 @@
-# preprocessing.py
+
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 
@@ -8,13 +8,13 @@ def load_data(file_path):
 
 def preprocess(df):
     """Clean and encode dataset."""
-    # Drop columns with all missing values
+   
     df = df.dropna(axis=1, how='all')
 
-    # Fill missing values with a placeholder
+   
     df = df.fillna("Unknown")
 
-    # Encode categorical (text/boolean) columns
+   
     for col in df.select_dtypes(include=['object', 'bool']).columns:
         df[col] = LabelEncoder().fit_transform(df[col])
 
