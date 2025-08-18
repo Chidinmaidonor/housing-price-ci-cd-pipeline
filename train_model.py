8b9c4061ee3d3aa7a220c65bc2feb1fb7d45e8d7
chidinma idonor <idonor.chidinma@gmail.com>
2025-08-18 10:02:09 -0500

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -4,21 +4,21 @@ from sklearn.linear_model import LinearRegression
 from preprocessing import load_data, preprocess
 
 def train():
-    # Load dataset
+
     df = load_data("data/housing.csv")
 
-    # Preprocess
+
     df = preprocess(df)
 
-    # Select features (X) and target (y)
+    
     X = df.drop(columns=["price"])
     y = df["price"]
 
-    # Train model
+    
     model = LinearRegression()
     model.fit(X, y)
 
-    # Save model
+
     joblib.dump(model, "model.pkl")
     print("Model trained and saved as model.pkl")
 
